463323abc98700d062e5371c9b264e06167dd2cc
LunaG0th <lev1dharren@gmail.com>
2019-09-25 20:24:37 +0800

    3

--- js/test.js
+++ js/test.js
@@ -1,13 +1,14 @@
 function gen () {
     let  input = document.querySelector('#input');
-    let  input2 = document.querySelector('#input2');
     let data = input.value.replace(/\r\n/g,"\n").split("\n")
-    let data2 = input2.value.replace(/\r\n/g,"\n").split("\n")
-    
     let  output = document.querySelector('#output');
-    let  output2 = document.querySelector('#output2');
     let newElement = document.createElement("p");
+
+    let  input2 = document.querySelector('#input2');
+    let data2 = input2.value.replace(/\r\n/g,"\n").split("\n")
+    let  output2 = document.querySelector('#output2');
     let newElement2 = document.createElement("p");
+    
     
     data.forEach((item) => {
         if (input.value != '') {
@@ -16,12 +17,18 @@ function gen () {
                 output.appendChild(newElement)
                 newElement.appendChild(document.createElement("br"))
         }
-        if (input2.value != '') {
-            
-            newElement2.append(`<meta name="keyword" content=" ${j},  \n "/>`);
-            output2.appendChild(newElement2)
-        }
     });
+
+
+    data2.forEach((item2) => {
+        if (input2.value != '') {
+            newElement2.append(` ${item2}, `);
+        }
+        output2.appendChild(newElement2)
+    });
+
+    newElement2.prepend('<meta name="keyword" content="');
+    newElement2.append('/>')
 
 }
 
