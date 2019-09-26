4a2ec29bc25859ea14b842e27ba5728c91a5d205
LunaG0th <lev1dharren@gmail.com>
2019-09-26 19:27:04 +0800

    5

--- js/test.js
+++ js/test.js
@@ -1,13 +1,13 @@
 function gen () {
     let  input = document.querySelector('#input');
-    let data = input.value.replace(/\r\n/g,"\n").split("\n")
+    let  data = input.value.replace(/\r\n/g,"\n").split("\n")
     let  output = document.querySelector('#output');
-    let newElement = document.createElement("p");
+    let  newElement = document.createElement("p");
 
     let  input2 = document.querySelector('#input2');
-    let data2 = input2.value.replace(/\r\n/g,"\n").split("\n")
+    let  data2 = input2.value.replace(/\r\n/g,"\n").split("\n")
     let  output2 = document.querySelector('#output2');
-    let newElement2 = document.createElement("p");
+    let  newElement2 = document.createElement("p");
     
 // pages
     data.forEach((item) => {
@@ -23,22 +23,26 @@ function gen () {
     data2.forEach((item2) => {
         if (input2.value != '') {
             newElement2.append(` ${item2}, `);
+            output2.appendChild(newElement2);
         }
-        output2.appendChild(newElement2)
+        
     });
 
     newElement2.prepend('<meta name="keyword" content="');
-    newElement2.append('/>')
+    newElement2.append(' "/>');
 
 }
 
 // select all text on click
-// data2.forEach((item2) => {
-//     if (input2.value != '') {
-//         item2 = item2.toLowerCase();
-//         newElement2.append(`<meta name="keyword" content="${item2},"/>`);
-//         output2.appendChild(newElement2)
-//         // newElement2.appendChild(document.createElement("br"))
-//     }
+// function SelectAll(id)
+// {
+//     document.getElementById(id).focus();
+//     document.getElementById(id).select();
+// }
 
-// });
+
+// Textarea:<br>
+// <textarea rows="3" id="txtarea" onClick="SelectAll('txtarea');" style="width:200px" >This text you can select all by clicking here </textarea>
+
+// Input TextBox:<br>
+// <input type="text" id="txtfld" onClick="SelectAll('txtfld');" style="width:200px" value = "This text you can select all" />
