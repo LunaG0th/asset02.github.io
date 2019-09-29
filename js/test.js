165de2c84422d62a233beeb724c65cf286ac5bc8
LunaG0th <lev1dharren@gmail.com>
2019-09-29 15:58:33 +0800

    9-22

--- js/test.js
+++ js/test.js
@@ -16,33 +16,61 @@ function gen () {
                 newElement.append(`<meta name="keyword" content="${item}"/>`);
                 output.appendChild(newElement)
                 newElement.appendChild(document.createElement("br"))
+                newElement.setAttribute('class', 'phrse2')
         }
     });
 
 //learn more
     data2.forEach((item2) => {
         if (input2.value != '') {
+            item2 = item2.toLowerCase();
             newElement2.append(` ${item2}, `);
             output2.appendChild(newElement2);
+            
         }
         
     });
 
     newElement2.prepend('<meta name="keyword" content="');
     newElement2.append(' "/>');
+    newElement2.setAttribute('class', 'phrse2')
 
 }
+// ==================================================
 
-// select all text on click
-// function SelectAll(id)
-// {
-//     document.getElementById(id).focus();
-//     document.getElementById(id).select();
-// }
+// case converter
+function cnvrt () {
+    let inputKey = document.querySelector('#key');
+    let keyArr = key.value.replace(/\r\n/g,"\n").split("\n");
+    let outputKey = document.querySelector('#outputKey');
+    let = newElem = document.createElement("p");
+    let caseSelect = document.querySelector('#optn').value
+    
+    if (caseSelect == "lwrCase") {
+        keyArr.forEach((str) => {
+            str= str.toLowerCase();
+            newElem.append(`${str} `);
+            outputKey.appendChild(newElem);
+            newElem.appendChild(document.createElement("br"))
+            newElem.setAttribute('id', 'phrse')
+        });
 
+    } else if (caseSelect == "uprCase") {
+        keyArr.forEach((str) => {
+            str= str.toUpperCase();
+            newElem.append(`${str} `);
+            outputKey.appendChild(newElem);
+            newElem.appendChild(document.createElement("br"))
+            newElem.setAttribute('id', 'phrse')
+        });
+    }
 
-// Textarea:<br>
-// <textarea rows="3" id="txtarea" onClick="SelectAll('txtarea');" style="width:200px" >This text you can select all by clicking here </textarea>
-
-// Input TextBox:<br>
-// <input type="text" id="txtfld" onClick="SelectAll('txtfld');" style="width:200px" value = "This text you can select all" />
+}
+function clearItem () {
+            let toRemov = document.querySelector('#phrse')
+            toRemov.parentNode.removeChild(toRemov);
+}
+function clearItem2 () {
+    let toRemov = document.querySelector('.phrse2')
+    toRemov.parentNode.removeChild(toRemov);
+}
